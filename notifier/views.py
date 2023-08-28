ae7d12598e34430186118591fb4a2eee4f0ab92d
ninuinu <helena.rosenzw@gmail.com>
2023-08-28 22:59:42 +0200

    init notifier app

---
+++ notifier/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# request handler
+
+def say_hello(request): 
+    return render(request, 'hello.html')
+
+
