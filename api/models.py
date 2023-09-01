e642b525c4ce4dcb96bc9b82cf014eaa33d946c3
ninuinu <helena.rosenzw@gmail.com>
2023-09-01 22:30:42 +0200

    add listing attributes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    url = models.TextField(null=False)
+    address = models.TextField(null=False)
+    price = models.IntegerField(null=False)
+    sqm_price = models.IntegerField()
+    sqm_size = models.TextField()
+    monthly_fee = models.IntegerField(null=False)
+    room_count = models.TextField(null=False)
+    image_url = models.TextField()
+    hemnet_listing_id = models.IntegerField(null=False)
+    date_published = models.DateTimeField(null=False)
+    location_id = models.IntegerField(null=False)
+    balcony = models.BooleanField()
+    patio = models.BooleanField()
+    floor = models.TextField()
+    elevator = models.TextField()
+
+    def __str__(self):
+        return self.title
