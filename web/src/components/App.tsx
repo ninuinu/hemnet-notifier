c6e7abdba19feddf18adc5a015373afb4d21c72a
ninuinu <helena.rosenzw@gmail.com>
2023-09-03 18:54:24 +0200

    add routes

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,8 +1,16 @@
-import { render } from "react-dom";
+import Home from "./Home";
+import Notify from "./Notify";
+import Search from "./Search";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App(props: any) {
-  return <div>Hello World! From react</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/notify" element={<Notify />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
-
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
