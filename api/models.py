648cafa8be2675ce1b6f02e05d70a6b7036ac46b
ninuinu <helena.rosenzw@gmail.com>
2023-09-03 22:00:29 +0200

    simplify listing model for now

--- api/models.py
+++ api/models.py
@@ -3,32 +3,17 @@ import string
 import random
 
 def generate_unique_id():
-    length = 6
+    length = 8
     while True:
-        id = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Listing.objects.filter(id=id).count() == 0:
+        hemnet_listing_id = ''.join(random.choices(string.digits, k=length))
+        if Listing.objects.filter(hemnet_listing_id=hemnet_listing_id).count() == 0:
             break
-    return id
+    return hemnet_listing_id
 
 
 # Create your models here.
 class Listing(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    url = models.TextField(null=False)
-    address = models.TextField(null=False)
-    price = models.IntegerField(null=False)
-    sqm_price = models.IntegerField()
-    sqm_size = models.TextField()
-    monthly_fee = models.IntegerField(null=False)
-    room_count = models.TextField(null=False)
-    image_url = models.TextField()
-    hemnet_listing_id = models.IntegerField(null=False)
-    date_published = models.DateTimeField(null=False)
-    location_id = models.IntegerField(null=False)
-    balcony = models.BooleanField()
-    patio = models.BooleanField()
-    floor = models.TextField()
-    elevator = models.TextField()
+    hemnet_listing_id = models.AutoField(primary_key=True, default=generate_unique_id, unique=True)
 
     def __str__(self):
-        return self.title
+        return self.hemnet_listing_id
