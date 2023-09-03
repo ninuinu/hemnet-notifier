f39f7a3c1a426840bfc89f047a74b99675e4da93
ninuinu <helena.rosenzw@gmail.com>
2023-09-03 22:02:10 +0200

    added sessions app and added new path to /api endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import ListingView
+from .views import TrackListingView
 
 urlpatterns = [
-    path('hello/', ListingView.as_view())
+    path('hello/', ListingView.as_view()),
+    path('track-listing', TrackListingView.as_view()),
+
 ]
