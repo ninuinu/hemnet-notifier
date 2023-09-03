0284e66ec257baa4d9418325ad7d2aefa0d85d1a
ninuinu <helena.rosenzw@gmail.com>
2023-09-03 22:01:09 +0200

    add post controller for creating listings

--- api/views.py
+++ api/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import ListingSerializer
+from rest_framework import generics, status
+from .serializers import ListingSerializer, TrackListingSerializer
 from .models import Listing
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
 class ListingView(generics.ListAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
-                  
+
+class TrackListingView(APIView):
+    serializer_class = TrackListingSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            hemnet_list_id = serializer.data.get('hemnet_listing_id')
+            queryset = Listing.objects.filter(hemnet_listing_id=hemnet_list_id)
+            if queryset.exists():
+                listing = queryset[0]
+                listing.hemnet_listing_id = hemnet_list_id
+                listing.save(update_fields=['hemnet_listing_id'])
+                return Response(ListingSerializer(listing).data, status=status.HTTP_200_OK)
+            else:
+                listing = Listing(hemnet_listing_id=hemnet_list_id)
+                listing.save()
+                return Response(ListingSerializer(listing).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+                   
