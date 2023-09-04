93b400bf0c41baaff38071523c78494fc0e2385a
ninuinu <helena.rosenzw@gmail.com>
2023-09-04 21:39:21 +0200

    set up basic outline of ListingCard component

---
+++ web/src/components/ListingCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import { Typography } from "@mui/material";
+
+export default function ListingCard(props: any) {
+  return (
+    <Card variant="outlined">
+      <Typography>Listing Card</Typography>
+      <Typography>Listing Card</Typography>
+      <Typography>Listing Card</Typography>
+    </Card>
+  );
+}
