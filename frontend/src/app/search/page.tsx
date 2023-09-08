fa3980b397dc61fd929b777fac6c1c01dce95091
ninuinu <helena.rosenzw@gmail.com>
2023-09-08 16:26:40 +0200

    add search bar for listing search

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,3 +1,14 @@
+import { Grid, TextField } from "@mui/material";
 export default function Page() {
-  return <h1>Search</h1>;
+  return (
+    <>
+      <div className="text-2xl pb-5">Search</div>
+      <Grid container spacing={12}>
+        <Grid item xs={4}>
+          <TextField fullWidth label="Address" id="fullWidth" />
+        </Grid>
+        <Grid item xs={8}></Grid>
+      </Grid>
+    </>
+  );
 }
