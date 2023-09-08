4c1524b21487ea28573e7f4a56fab32b46aa1a83
ninuinu <helena.rosenzw@gmail.com>
2023-09-08 16:10:28 +0200

    remove unused dir

--- frontend/src/app/dashboard/create/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>Dashboard/Create</h1>;
-}
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <Link href="/dashboard/create">/Create</Link>
     </div>
   );
 }
