23c937f9eb8ecd46d6225d18688404a12f9c5f44
ninuinu <helena.rosenzw@gmail.com>
2023-09-10 22:39:21 +0200

    add listing pagination

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,7 +1,22 @@
 import ListingCard from "@/components/ListingCard";
+import getAllListings from "@/lib/api/getAllListings";
 import { Grid, TextField } from "@mui/material";
-export default function Page() {
-  const listings = ["Drottninggatan 32", "Hornsgatan 12", "Björkvägen 9"];
+import Link from "next/link";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const page =
+    typeof searchParams.page === "string" ? Number(searchParams.page) : 1;
+  const limit =
+    typeof searchParams.limit === "string" ? Number(searchParams.limit) : 3;
+
+  const listings: any = await getAllListings(page, limit);
+
+  const start = (Number(page) - 1) * Number(limit);
+
   return (
     <>
       <div className="text-2xl pb-5">Search</div>
@@ -11,16 +26,34 @@ export default function Page() {
         </Grid>
         <Grid item xs={8}>
           <Grid container direction="column" item xs={12} spacing={4}>
-            {listings.map((address) => {
+            {listings.map((listing: any) => {
               return (
                 <Grid item xs={8}>
-                  <ListingCard address={address}></ListingCard>
+                  <ListingCard listing={listing}></ListingCard>
                 </Grid>
               );
             })}
           </Grid>
         </Grid>
       </Grid>
+      <div className="flex space-x-6 text-2xl">
+        <Link
+          href={`/search?page=${page > 1 ? page - 1 : 1}`}
+          className={
+            page > 1
+              ? "rounded border bg-gray-100 px-3 py-1 text-m text-gray-8"
+              : "rounded border bg-gray-100 px-3 py-1 text-m text-gray-8 opacity-40 pointer-events-none"
+          }
+        >
+          Previous
+        </Link>
+        <Link
+          href={`/search?page=${page + 1}`}
+          className={"rounded border bg-gray-100 px-3 py-1 text-m text-gray-8"}
+        >
+          Next
+        </Link>
+      </div>
     </>
   );
 }
