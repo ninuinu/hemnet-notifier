f26eb682bb1ab5726ee8c4d08e96db460a7de9c5
ninuinu <helena.rosenzw@gmail.com>
2023-09-10 22:30:19 +0200

    move lib functions into api sub directory for improved structure

--- frontend/lib/getAllListings.tsx
+++
@@ -1,7 +0,0 @@
-export default async function getAllListings() {
-  const res = await fetch("http://localhost:3001/listing/");
-
-  if (!res.ok) throw new Error("Failed to fetch listings");
-
-  return res.json();
-}
