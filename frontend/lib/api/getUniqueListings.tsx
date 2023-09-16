368ea743452495793146599f8ec3042789b20aad
ninuinu <helena.rosenzw@gmail.com>
2023-09-16 22:55:15 +0200

    add fetch function for calling unique listings

---
+++ frontend/lib/api/getUniqueListings.tsx
@@ -0,0 +1,15 @@
+export default async function getAllUniqueListings(
+  page?: number,
+  limit?: number
+) {
+  let params = "";
+  if (page && limit) {
+    params = `?page=${page}&limit=${limit}`;
+  }
+
+  const res = await fetch(`http://localhost:3001/listing/unique/${params}`);
+
+  if (!res.ok) throw new Error("Failed to fetch listings");
+
+  return res.json();
+}
